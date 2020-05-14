dcddb5aa1b67855b8294fec1c1d9426b55b8cbac
William Rave <wrave@gwu.edu>
2020-05-14 10:24:17 -0400

    Inital commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name, devoured) VALUES ("Quarter pounder", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Extra-Pickles", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Bacon Burger", true);
